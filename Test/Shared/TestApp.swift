83aeb318a557c6ab5786569fdab01b12987efbb2
天夢橋 <kobukuro2001898@gmail.com>
2022-03-14 22:27:42 +0900

    Swift追加

---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by 中根真 on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
